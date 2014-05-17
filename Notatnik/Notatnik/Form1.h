fef7c6050c168702060aef372cec1cbb02d471c7
Szymon <srybka@ydp.eu>
2014-05-17 09:59:58 +0200

    Nowy projekt

---
+++ Notatnik/Notatnik/Form1.h
@@ -0,0 +1,118 @@
+#pragma once
+
+namespace WindowsFormApplication1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^  plikToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  nowyToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  zako—czToolStripMenuItem;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->plikToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->nowyToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->zako—czToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->plikToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(284, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// plikToolStripMenuItem
+			// 
+			this->plikToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->nowyToolStripMenuItem,
+					this->zako—czToolStripMenuItem
+			});
+			this->plikToolStripMenuItem->Name = L"plikToolStripMenuItem";
+			this->plikToolStripMenuItem->Size = System::Drawing::Size(38, 20);
+			this->plikToolStripMenuItem->Text = L"Plik";
+			// 
+			// nowyToolStripMenuItem
+			// 
+			this->nowyToolStripMenuItem->Name = L"nowyToolStripMenuItem";
+			this->nowyToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->nowyToolStripMenuItem->Text = L"Nowy";
+			// 
+			// zako—czToolStripMenuItem
+			// 
+			this->zako—czToolStripMenuItem->Name = L"zako—czToolStripMenuItem";
+			this->zako—czToolStripMenuItem->ShortcutKeys = static_cast<System::Windows::Forms::Keys>((System::Windows::Forms::Keys::Alt | System::Windows::Forms::Keys::F4));
+			this->zako—czToolStripMenuItem->Size = System::Drawing::Size(160, 22);
+			this->zako—czToolStripMenuItem->Text = L"ZakoÒcz";
+			this->zako—czToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::zako—czToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void zako—czToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+
+				 Close();
+	}
+	};
+}
+
